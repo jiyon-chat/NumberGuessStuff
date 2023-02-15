829be1f670bd5df32182898accf76e003f93f1fe
jiyon-chat <90795395+jiyon-chat@users.noreply.github.com>
2023-02-15 09:31:44 -0500

    Update RunNumberGuess.java
    refactor with difficulties, replayable

--- src/RunNumberGuess.java
+++ src/RunNumberGuess.java
@@ -1,17 +1,28 @@
-//©Jiyon Chatterjee's Number Guess Game, 2023
+//©Jiyon Chatterjee's Number Guess Game Refactor, 2023
 import java.util.Scanner;
 import java.util.ArrayList;
 
-public class RunNumberGuess {
-    public static void main(String[] args) { //makes user enter range for guess game
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the lower bound of the number guess range: ");
-        int lowerBound = errorCheck(scanner);
-        System.out.print("Enter the upper bound of the number guess range: ");
-        int upperBound = errorCheck(scanner);
-        int correct = (int) (Math.random() * (upperBound - lowerBound + 1) + lowerBound); //generates random integer within user-created range
-        ArrayList<Integer> guesses = new ArrayList<Integer>(); //creates Array List to keep track of guesses
-        int guessCount = 0;
+public class RunNumberGuessGame {
+     int lowerBound;
+     int upperBound;
+     int correct;
+     ArrayList<Integer> guesses;
+     int guessCount;
+
+    public RunNumberGuessGame(int lowerBound, int upperBound) {
+        this.lowerBound = lowerBound;
+        this.upperBound = upperBound;
+        this.correct = (int) (Math.random() * (upperBound - lowerBound + 1) + lowerBound);
+        this.guesses = new ArrayList<>();
+        this.guessCount = 0;
+    }
+
+    public void playGame(Scanner scanner, String difficulty) { 
+        if (difficulty.equals("easy")) { //sets difficulties
+            upperBound = upperBound + 10;
+        } else if (difficulty.equals("medium")) {
+            upperBound = upperBound + 5;
+        }
 
         while (true) {
             System.out.print("Enter your guess please: ");
@@ -21,7 +32,7 @@ public class RunNumberGuess {
             } else { //adds guess to Array List and counts it
                 guesses.add(guess);
                 guessCount++;
-                if (guess == correct) { //if user guesses right, tells them their number of guesses
+                if (guess == correct) {
                     System.out.println("Congrats pal! You guessed the correct number in " + guessCount + " tries.");
                     break;
                 } else if (guess < correct) {
@@ -32,11 +43,34 @@ public class RunNumberGuess {
             }
         }
     }
-    private static int errorCheck(Scanner scanner){ //error checking method
-        while(!scanner.hasNextInt()){
+
+    private static int errorCheck(Scanner scanner) { //error checking method
+        while (!scanner.hasNextInt()) {
             System.out.print("That's an invalid input. Please enter an integer: ");
             scanner.next();
         }
         return scanner.nextInt();
+    }
+
+    public static void main(String[] args) { //makes user enter range for guess game
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("Enter the lower bound of the number guess range: ");
+            int lowerBound = errorCheck(scanner);
+            System.out.print("Enter the upper bound of the number guess range: ");
+            int upperBound = errorCheck(scanner);
+            System.out.print("Choose difficulty level (easy, medium, or hard): ");
+            String difficulty = scanner.next();
+
+            RunNumberGuessGame game = new RunNumberGuessGame(lowerBound, upperBound);
+            game.playGame(scanner, difficulty);
+
+            System.out.print("Do you want to play again? (yes/no): "); //makes game replayable
+            String playAgain = scanner.next();
+            if (playAgain.equals("no")) {
+                break;
+            }
+        }
+        System.out.println("Thanks for playing!");
     }
 }
